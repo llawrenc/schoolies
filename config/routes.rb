cb596d72f24b8933d131141fea3a3621247c3678
llawrenc <lloydo@lawrenceau.net>
2020-02-19 19:55:02 +1000

    Devise modify users path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :locations
   resources :subcategories
   resources :categories
-  devise_for :users
+  devise_for :users, path: ''
   resources :reports
   root 'dashboard#index'
 end
