2a08daadaaa0d90fdb8589c1aa2a9d68cb30418c
llawrenc <lloydo@lawrenceau.net>
2020-02-24 10:55:34 +1000

    Update 20200217113737_create_reports.rb

--- db/migrate/20200217113737_create_reports.rb
+++ db/migrate/20200217113737_create_reports.rb
@@ -4,8 +4,8 @@ class CreateReports < ActiveRecord::Migration[5.2]
       t.string :title
       t.text :description
       t.date :date_occurred
-      t.category_id :integer
-      t.subcategory_id :integer
+      t.integer :category_id
+      t.integer :subcategory_id
 
       t.timestamps
     end
